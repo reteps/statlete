c4c6a07c811c32fb6fa985001c9447079ee710df
Peter Stenger <peter.a.stenger@gmail.com>
2019-05-10 10:50:38 -0400

    Added New Stuff

---
+++ Statlete/Statlete/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Statlete
+//
+//  Created by Peter Stenger on 5/8/19.
+//  Copyright © 2019 Peter Stenger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
